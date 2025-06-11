a23fbb25b560e0e46c957a6847a44b8fa8f00c19
ivcx <ivcx99@gmail.com>
2025-06-11 17:43:10 +0300

    a

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
+  linkActiveClass: "active",
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -12,33 +13,21 @@ const router = createRouter({
     {
       path: '/celi',
       name: 'celi',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/CeliView.vue'),
     },
     {
       path: '/bizness',
       name: 'bizness',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Bizness.vue'),
     },
     {
       path: '/bagatiba',
       name: 'bagatiba',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Bagati.vue'),
     },
         {
       path: '/nepatiesas_zinas',
       name: 'nepatiesas_zinas',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/NepatiesasZinas.vue'),
     },
   ],
