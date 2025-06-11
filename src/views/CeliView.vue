17e4bf109a5bf4214d0fa6e4894f5c01b66e3b03
ivcx <ivcx99@gmail.com>
2025-06-11 23:34:22 +0300

    b

--- src/views/CeliView.vue
+++ src/views/CeliView.vue
@@ -2,28 +2,53 @@
 
 </script>
 
-<template>
-    <div class="home">
-      <section class="article bg-white shadow-md rounded-lg p-6 mb-6">
-        <h2 class="text-2xl font-bold mb-4">Strādnieki gaida ielas pabeikšanu lai atkal varētu to uzrtakt.</h2>
-        <p class="text-gray-700 mb-4">
-          Šogad ievilkušies Rīgas ceļu remontdarbi rada problēmas ūdens un kanalizāciajs atjaunošanas brigādēm. Pēc Rīgas domes noslēgtā līguma ceļa atjaunošana vajadzēja 
-          beigties pirms trim mēnešiem, taču darbi dažādu iemeslu dēļ ir jau projām palikuši nepadarīti. Rīgas dome taisnojas ar to ka neparedzētās lietavas
-           radījušas papildus sarežģījumus darbu izpildei.
-        </p>
-        <p class="text-gray-700 mb-4">
-          Taču nu šī problēma ir sasniegusi tālāku soli. Jau trīs gadus paredzētie ūdens un kanalizācijas darbi, kuriem bija jāsākas 
-          pagājušās nedēļas ceturdienā tagad stāv uz vietas. Ierēdņi apgalvo ka likumiski nav iespējams apvienot darbus un trubu nomaiņa 
-          jau izraktajā ielā nav iespējama. 
-        </p>
-        <p class="text-gray-700">
-          "Es jau grib rakt vaļā, bet šie čamājas" , saka būvnieks no SIA ZeltaTrubas brigādes.
-        </p>
-        <p>"Es cenšos mierīgi strādāt, bet šie te sēž un mani kritizē." , apgalvoja brigādes BedresUnBedres biedrs.</p>
-        <p>
-          Rīgas dome apgalvo, ka tagad būs iespējams ietaupīt naudu uz ceļa segumu, diemžēl pilnā summa jau ir samaksāta, līdz ar to 
-          finanses tiks investētas uzņēmuma BedresUnBedres jaunajās Mersedes mašīnās. 
-        </p>
-      </section>
-    </div>
-</template>
+<template> 
+    <div class="home"> 
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-white shadow-md rounded-lg p-6 mb-6"> 
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Strādnieki gaida ielas pabeikšanu lai atkal varētu to uzrtakt.</h2> 
+        <p itemprop="articleBody" class="text-gray-700 mb-4"> 
+          Šogad ievilkušies Rīgas ceļu remontdarbi rada problēmas ūdens un kanalizāciajs atjaunošanas brigādēm. Pēc Rīgas domes noslēgtā līguma ceļa atjaunošana vajadzēja  
+          beigties pirms trim mēnešiem, taču darbi dažādu iemeslu dēļ ir jau projām palikuši nepadarīti. Rīgas dome taisnojas ar to ka neparedzētās lietavas 
+          radījušas papildus sarežģījumus darbu izpildei. 
+        </p> 
+
+        <p itemprop="articleBody" class="text-gray-700 mb-4"> 
+          Taču nu šī problēma ir sasniegusi tālāku soli. Jau trīs gadus paredzētie ūdens un kanalizācijas darbi, kuriem bija jāsākas  
+          pagājušās nedēļas ceturdienā tagad stāv uz vietas. Ierēdņi apgalvo ka likumiski nav iespējams apvienot darbus un trubu nomaiņa  
+          jau izraktajā ielā nav iespējama.  
+        </p> 
+
+        <div itemscope itemtype="https://schema.org/Quotation" class="inline-flex px-2 pb-4 w-full text-gray-700">
+          <p>"Es jau grib rakt vaļā, bet šie čamājas"</p>
+          <div itemprop="spokenByCharacter" itemscope itemtype="https://schema.org/Organization" class="inline-flex">
+            <p class=""> saka būvnieks no </p>
+            <p class="px-1" itemprop="legalName"> SIA ZeltaTrubas</p> 
+            <p class="px-1">brigādes. </p>
+	        </div> 
+        </div>
+
+        <div itemscope itemtype="https://schema.org/Quotation" class="inline-flex px-2 pb-4 w-full text-gray-700">
+           <p>"Es cenšos mierīgi strādāt, bet šie te sēž un mani kritizē." </p>
+          <div itemprop="spokenByCharacter" itemscope itemtype="https://schema.org/Organization" class="inline-flex">
+            <p class=""> apgalvoja darbinieks no uzņēmuma </p>
+            <p class="px-1" itemprop="legalName"> BedresUnBedres</p> 
+	        </div> 
+        </div>
+
+        <p class="text-gray-700 mb-4"> 
+          Rīgas dome apgalvo, ka tagad būs iespējams ietaupīt naudu uz ceļa segumu, diemžēl pilnā summa jau ir samaksāta, līdz ar to  
+          finanses tiks investētas uzņēmuma BedresUnBedres jaunajās Mersedes mašīnās.  
+        </p> 
+       <span class=" text-sm" itemprop="datePublished" content="2020-12-06T08:00:00+08:00"> 
+          Decembris 6, 2020 
+        </span> 
+      </section> 
+      <section> 
+        <div class="inline-flex w-full p-4"  itemprop="author" itemscope itemtype="https://schema.org/Person" > 
+          <p class="text-gray-500 px-1">Darba autors:</p>  
+          <p class="text-gray-500 px-1" itemprop="givenName">Ivo</p> 
+          <p class="text-gray-500 " itemprop="familyName">Konons</p> 
+        </div>  
+      </section> 
+    </div> 
+</template> 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,54 +10,54 @@
       </div>
     </div>
     <div class="home grid grid-cols-4 gap-2">
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Strādnieki gaida ielas pabeikšanu lai atkal varētu to uzrtakt.</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Strādnieki gaida ielas pabeikšanu lai atkal varētu to uzrtakt.</h2>
         <RouterLink class="text-sm hover:text-rose-700" to="/celi">Lasīt vairāk</RouterLink>
       </section>
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Proaktīvi jaunieši izmet ārā savu naudu lai pierastu pie sajūtām nākotnei</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Proaktīvi jaunieši izmet ārā savu naudu lai pierastu pie sajūtām nākotnei</h2>
         <RouterLink class="text-sm hover:text-rose-700" to="/bizness">Lasīt vairāk</RouterLink>
       </section>
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Kā izmantot slimības lai palielinātu valsts vidējos ekonomiskos rādītājus</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
+        <h2 itemtype="headline" class="text-2xl font-bold mb-4">Kā izmantot slimības lai palielinātu valsts vidējos ekonomiskos rādītājus</h2>
         <RouterLink class="text-sm hover:text-rose-700" to="/bagatiba">Lasīt vairāk</RouterLink>
       </section>
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
         <RouterLink class="text-sm hover:text-rose-700" to="/bagatiba">Nevaig Lasīt vairāk</RouterLink>
       </section>
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 row-span-2 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Šī lapa saņem apbalvojumus par visu laiku nepatiesākajām ziņām Latvijas teritorijā</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 row-span-2 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Šī lapa saņem apbalvojumus par visu laiku nepatiesākajām ziņām Latvijas teritorijā</h2>
         <p>Šī lapa bieži tiek atzīta par vienu no vadošajām platformām nepatiesu ziņu izplatīšanā Latvijas teritorijā. Pateicoties mērķtiecīgai dezinformācijas kampaņai un klikšķu ēsmas virsrakstiem, tā ir ieguvusi apšaubāmu slavu kā uzticama vieta maldināšanai. Tās saturs ne tikai izkropļo faktus, bet arī bieži vien rada sensācijas, kas var ietekmēt sabiedrisko viedokli un radīt nepareizu priekšstatu par aktuāliem notikumiem. Šīs lapas apbalvojumi par "nepatiesākajām ziņām" liecina par nepieciešamību rūpīgi izvērtēt informācijas avotus un neuzticēties neapstiprinātiem ziņojumiem.</p>
-        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas">Nevaig Lasīt vairāk</RouterLink>
+        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas_balva">Nevaig Lasīt vairāk</RouterLink>
       </section>
-            <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-3 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
-        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas">Nevaig Lasīt vairāk</RouterLink>
+            <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-3 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
+        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas_balva">Nevaig Lasīt vairāk</RouterLink>
       </section>
 
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
         <RouterLink class="text-sm hover:text-rose-700" to="/celi">Lasīt vairāk</RouterLink>
       </section>
 
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6  hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
-        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas">Lasīt vairāk</RouterLink>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6  hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Lapa kura paredzēta vietas aizpildīšanai</h2>
+        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas_balva">Lasīt vairāk</RouterLink>
       </section>
 
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 row-span-2 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Kā izmantot slimības lai palielinātu valsts vidējos ekonomiskos rādītājus</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 row-span-2 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Kā izmantot slimības lai palielinātu valsts vidējos ekonomiskos rādītājus</h2>
         <RouterLink class="text-sm hover:text-rose-700" to="/bagatiba">Lasīt vairāk</RouterLink>
       </section>
 
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Proaktīvi jaunieši izmet ārā savu naudu lai pierastu pie sajūtām nākotnei</h2>
-        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas">Lasīt vairāk</RouterLink>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 hover:border-2 hover:border-black">
+        <h2  itemprop="headline" class="text-2xl font-bold mb-4">Proaktīvi jaunieši izmet ārā savu naudu lai pierastu pie sajūtām nākotnei</h2>
+        <RouterLink class="text-sm hover:text-rose-700" to="/nepatiesas_zinas_balva">Lasīt vairāk</RouterLink>
       </section>
 
-      <section class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
-        <h2 class="text-2xl font-bold mb-4">Strādnieki gaida ielas pabeikšanu lai atkal varētu to uzrtakt.</h2>
+      <section itemscope itemtype="https://schema.org/Article" class="article bg-neutral-100 shadow-md rounded-lg p-6 mb-6 col-span-2 hover:border-2 hover:border-black">
+        <h2 itemprop="headline" class="text-2xl font-bold mb-4">Strādnieki gaida ielas pabeikšanu lai atkal varētu to uzrtakt.</h2>
         <RouterLink class="text-sm hover:text-rose-700" to="/bizness">Lasīt vairāk</RouterLink>
       </section>
     </div>
